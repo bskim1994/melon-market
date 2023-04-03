0f0ffce898dadc9c8c49229c74fec7431f9d1858
bskim1994 <akfdml10@kakao.com>
2023-04-03 22:01:16 +0900

    tailwind 적용 및 header, sidebar, dashboard 구현

---
+++ fe-melon-market/src/App.js
@@ -0,0 +1,16 @@
+import Dashboard from './view/dashboard';
+import Header from './view/header';
+import Sidebar from './view/sidebar';
+
+function App() {
+  return (
+    <div>
+      
+      <Header/>
+      <Sidebar/>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
